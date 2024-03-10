de4597c14c1e02c8712346f747c9294209c4a553
joseph0305 <74284597+joseph0305@users.noreply.github.com>
2024-03-10 19:56:00 +0900

    Create 백준_24265.py

---
+++ 백준_24265.py
@@ -0,0 +1,21 @@
+
+#(n-1)+(n-2)...+1
+
+n = int(input())
+sum = 0
+for i in range(1, n):
+  sum += i
+
+print(sum)
+print(2)
+
+# 다른 코드 
+# N=int(input())
+# print(N*(N-1)//2)
+# print(2)
+
+#  why??
+# 잘 설명한 블로그
+#https://rightbellboy.tistory.com/204
+
+# 시그마 수학 개념 복습 필요
