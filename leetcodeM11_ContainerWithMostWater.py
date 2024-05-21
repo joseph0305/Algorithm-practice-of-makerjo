32583b27a2953dab2464c0f56c866049835b4892
joseph0305 <74284597+joseph0305@users.noreply.github.com>
2024-05-21 07:57:33 +0000

    M11-2

--- leetcodeM11_ContainerWithMostWater.py
+++ leetcodeM11_ContainerWithMostWater.py
@@ -1,41 +1,40 @@
 class Solution:
-  def maxArea(self, height: List[int]) -> int:
 
-      l = len(height)
-      d_height = enumerate(height)
+    def maxArea(self, height: List[int]) -> int:
 
-      for i, d in d_height:
-          print(i, d)
+        l = len(height)
+        d_height = enumerate(height)
 
-          #종료조건 : l * height를 했을 때 지금 max에 들어가있는 값보다 무조건 작을 때
-          #그전까지는 두개씩 찝어서 계산 : index 있고 두개 높이 있으니까 가능
+        for i, d in d_height:
+            print(i, d)
 
+            #종료조건 : l * height를 했을 때 지금 max에 들어가있는 값보다 무조건 작을 때
+            #그전까지는 두개씩 찝어서 계산 : index 있고 두개 높이 있으니까 가능
 
-      return
+        return
 
+    #min(s), max(s) : 시간 복잡도 :  O(n)
 
-      # le = len(height)
-      # count = 0
-      # # 나보다 w 가 작은데 h도 작은거는 계산할 필요 X
-      # # 이전거보다 h가 큰 것 만 find
-      # for i in range(le):
-      #     for w in range(le-1,i,-1):
+    # le = len(height)
+    # count = 0
+    # # 나보다 w 가 작은데 h도 작은거는 계산할 필요 X
+    # # 이전거보다 h가 큰 것 만 find
+    # for i in range(le):
+    #     for w in range(le-1,i,-1):
 
+    #return 0
+    # count = 0
+    # re_arr = []
+    # le = len(height)
+    # for i in range(le):
+    #     for w in range(1,le):
+    #         if i+w >= le:
+    #             break
+    #         re_arr.append(w * min(height[i],height[i+w]))
+    #         count += 1
+    # print("c2",count)
+    # return max(re_arr)
 
+    # 7 3 3 8
 
-
-
-      #return 0
-      # count = 0
-      # re_arr = []
-      # le = len(height)
-      # for i in range(le):
-      #     for w in range(1,le):
-      #         if i+w >= le:
-      #             break  
-      #         re_arr.append(w * min(height[i],height[i+w]))
-      #         count += 1
-      # print("c2",count)
-      # return max(re_arr)
-
-      # 7 3 3 8 
+    #https://hongcana.tistory.com/41
